5d3a963d40047d1f3572243c4faa3828726baabb
Emily <echeun17@mail.ccsf.edu>
2019-10-06 02:50:43 -0700

    update Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
-    return (
-        <div>
-            NavBar
-        
-        </div>
-    )
-}
+  return (
+    <div className='NavBar'>
+      <Link to="/login" className='NavBar-link'>LOG IN</Link>
+      &nbsp;&nbsp;|&nbsp;&nbsp;
+      <Link to="/signup" className='NavBar-link'>SIGN UP</Link>
+    </div>
+  );
+};
 
 export default NavBar;
