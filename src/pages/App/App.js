8c4eadf6670d50cf8ae697c449fedf5a3c97ae58
Emily <echeun17@mail.ccsf.edu>
2019-10-06 03:37:41 -0700

    update App state

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,29 +4,40 @@ import { Route, Switch } from 'react-router-dom';
 import SignupPage from '../SignupPage/SignupPage';
 import LoginPage from '../LoginPage/LoginPage';
 import NavBar from '../../components/NavBar/NavBar';
+import customerService from '../../utils/customerService';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // Initialize user if there's a token, otherwise null
+      customer: customerService.getCustomer()
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <NavBar />
+          <NavBar 
+            customer={this.state.customer}
+          />
         </header>
 
         <Switch>
-          <Route exact path='/signup' render={({ history }) => 
+          <Route exact path='/signup' render={({ history }) =>
             <SignupPage
               history={history}
-              
+
             />
-          }/>
-          <Route exact path='/login' render={() => 
+          } />
+          <Route exact path='/login' render={() =>
             <LoginPage
-              
+
             />
-          }/>
-      
+          } />
+
         </Switch>
       </div>
     );
