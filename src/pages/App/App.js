098d857855f36b75305cba3794c98c18100278fc
Emily <echeun17@mail.ccsf.edu>
2019-10-06 02:51:31 -0700

    convert App.js to class, added routes to signup/login

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 import NavBar from '../../components/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <NavBar />
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+
+        <Switch>
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              
+            />
+          }/>
+          <Route exact path='/login' render={() => 
+            <LoginPage
+              
+            />
+          }/>
+      
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
