8ab6230433a8e1c741da384e6c0e9d4e4946deef
Emily <echeun17@mail.ccsf.edu>
2019-10-06 02:53:30 -0700

    added customerService

---
+++ src/utils/customerService.js
@@ -0,0 +1,19 @@
+const BASE_URL = '/api/customers/';
+
+function signup(customer) {
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify(customer)
+  })
+  .then(res => {
+    if (res.ok) return res.json();
+    // Probably a duplicate email
+    throw new Error('Email already taken!');
+  })
+  .then(data => data);
+}
+
+export default {
+  signup
+};
