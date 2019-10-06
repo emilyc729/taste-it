da8ec7519cd5988e8b20048a0c266b3a25a51a8e
Emily <echeun17@mail.ccsf.edu>
2019-10-06 03:38:21 -0700

    update functions w/ token

--- src/utils/customerService.js
+++ src/utils/customerService.js
@@ -1,3 +1,5 @@
+import tokenService from './tokenService';
+
 const BASE_URL = '/api/customers/';
 
 function signup(customer) {
@@ -11,9 +13,16 @@ function signup(customer) {
     // Probably a duplicate email
     throw new Error('Email already taken!');
   })
-  .then(({token}) => token);
+  .then(({token}) => {
+    tokenService.setToken(token);
+  });
+}
+
+function getCustomer() {
+  return tokenService.getCustomerFromToken();
 }
 
 export default {
-  signup
+  signup,
+  getCustomer,
 };
--- src/utils/tokenService.js
+++ src/utils/tokenService.js
@@ -1,11 +1,32 @@
 function setToken(token) {
-    if(token) {
+    if (token) {
         localStorage.setItem('token', token);
     } else {
         localStorage.removeItem('token');
     }
 }
 
+function getToken() {
+    let token = localStorage.getItem('token');
+    if (token) {
+        // Check if expired, remove if it is
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        // JWT's exp is expressed in seconds, not milliseconds, so convert
+        if (payload.exp < Date.now() / 1000) {
+            localStorage.removeItem('token');
+            token = null;
+        }
+    }
+    return token;
+}
+
+function getCustomerFromToken() {
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).customer : null;
+}
+
 export default {
     setToken,
+    getToken,
+    getCustomerFromToken
 };
