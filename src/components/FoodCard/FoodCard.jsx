02407fa2cc2b3dc84ca9fff23416fef92cc9debd
Emily <echeun17@mail.ccsf.edu>
2019-10-07 17:07:25 -0700

    display food items

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './FoodCard.css';
+
+
+const FoodCard = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                {props.foods.map((food, idx) =>
+                    <div key={food.name} className="col-md-4">
+                        <div className="card bg-dark mt-4">
+                            <img src={food.food_photo} className="card-img-top" alt={food.description} />
+                                <div className="card-body bg-light">
+                                    <h5 className="card-title d-flex justify-content-between">
+                                        <span>{food.name}</span>
+                                        <span>${food.price}</span>
+                                    </h5>
+                                    <div className="card-text text-center d-flex justify-content-around">
+                                        <div>
+                                            <button className="btn"><i className="far fa-minus-square"></i></button>
+                                            <span className="quantity">0</span>
+                                            <button className="btn"><i className="far fa-plus-square"></i></button>
+                                        </div>
+                                        <button className="btn btn-outline-primary">Add to Order</button>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+        
+export default FoodCard;
