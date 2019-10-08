aa167d436fb035862dd6e60d2dc16cd8c5c8fdb3
Emily <echeun17@mail.ccsf.edu>
2019-10-08 11:30:34 -0700

    add orders api

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/customers', require('./routes/api/customers'));
 
+app.use('/api/orders', require('./routes/api/orders'));
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
