07c64b7c60a2bdaaaef54bfe2c92e6baab36a1f2
Emily <echeun17@mail.ccsf.edu>
2019-10-08 14:30:04 -0700

    added create, delete order

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,13 +1,35 @@
 const Customer = require('../models/customer');
 
 module.exports = {
-   create
-  };
+  index,
+  create,
+  delete: deleteOne
+};
+
+async function index(req, res) {
+  console.log('--controller---');
+  console.log(req.user);
+
+  const customer = await Customer.findById(req.user._id);
+  const orders = customer.orders;
+  return res.status(200).json(orders);
+}
 
 async function create(req, res) {
-    console.log(req.user);
-    const customer = await Customer.findById(req.user._id);
-    customer.orders.push(req.body);
-    const saveCustomer = await customer.save();
-    return res.json(saveCustomer);
-  }
+  //console.log(req.user);
+  const customer = await Customer.findById(req.user._id);
+  customer.orders.push(req.body);
+  const saveCustomer = await customer.save();
+  return res.json(saveCustomer);
+}
+
+async function deleteOne(req, res) {
+  const customer = await Customer.findById(req.user._id);
+  customer.orders.forEach(function(order, idx) {
+    if(req.params.id === order.id) {
+      const deletedOrder = customer.orders.splice(idx, 1);
+      customer.save();
+      return res.json(deletedOrder);
+    }
+  })
+}
--- routes/api/orders.js
+++ routes/api/orders.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const ordersCtrl = require('../../controllers/orders');
 
 
-
 router.use(require('../../config/auth'));
+router.get('/', ordersCtrl.index);
 router.post('/', ordersCtrl.create);
+router.delete('/:id', ordersCtrl.delete);
 
 module.exports = router;
