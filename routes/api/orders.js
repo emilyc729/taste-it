f59f185aa7f043ebc0063436429dc326b57b1757
Emily <echeun17@mail.ccsf.edu>
2019-10-08 16:05:42 -0700

    added helper func

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -8,4 +8,10 @@ router.get('/', ordersCtrl.index);
 router.post('/', ordersCtrl.create);
 router.delete('/:id', ordersCtrl.delete);
 
+/*----- Helper Functions -----*/
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({ msg: 'Not Authorized' });
+}
+
 module.exports = router;
