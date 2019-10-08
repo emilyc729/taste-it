4675893f18174bcee12c435cca3e172f6728a32c
Emily <echeun17@mail.ccsf.edu>
2019-10-07 17:07:11 -0700

    added restaurant menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Menu.css';
+import FoodCard from '../FoodCard/FoodCard';
+
+const Menu = (props) => {
+    return (
+        <div className='Menu'>
+            {props.categories.map((category, idx) =>
+                <div key={category.name}>
+                    <h3 className="text-center mt-4" name={category.name}>{category.name}</h3>
+                    <FoodCard foods={category.foods} />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Menu;
