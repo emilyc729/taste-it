9ab0a7dfbc5841ebecaaa7bdaf6e9cdf5a16ef60
Emily <echeun17@mail.ccsf.edu>
2019-10-08 16:05:33 -0700

    removed uneeded code

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -8,14 +8,4 @@ router.post('/login', customerCtrl.login);
 
 
 
-/*---------- Protected Routes ----------*/
-//router.use(require('../../config/auth'));
-
-/*----- Helper Functions -----*/
-function checkAuth(req, res, next) {
-    if (req.user) return next();
-    return res.status(401).json({ msg: 'Not Authorized' });
-}
-
-
 module.exports = router;
