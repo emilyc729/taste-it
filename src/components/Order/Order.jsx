c0f3042c7ba28e1186c2dcceee0eca02dd990e5d
Emily <echeun17@mail.ccsf.edu>
2019-10-13 20:12:37 -0700

    removed commented out code

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -5,54 +5,6 @@ import './Order.css';
 
 
 class Order extends Component {
-    state = {
-       // quantity: this.props.food.quantity,
-        //order_foodlist: []
-
-    }
-
-    // async componentDidMount() {
-    //     const order_foodlist = await foodsApi.getAllFoods(this.props.food.restaurant_id);
-    //     this.setState({order_foodlist: order_foodlist});
-    // }
-
-    increaseQuantity = (order_id, food_id, curQuantity) => {
-        //increase quantity by 1
-        //let quantity = curQuantity + 1
-        //const updatedFood = foodApi.updateFood(food_id, quantity);
-        //this.setState({order_foodlist: updateFood})
-    }
-
-    decreaseQuantity = (food_id) => {
-        //decrease quantity by 1
-        //let quantity = curQuantity - 1
-        //const updatedFood = foodApi.updateFood(food_id, quantity);
-        //this.setState({order_foodlist: updateFood})
-    }
-
-
-
-    // increment = async () => {
-    //     let addOne = this.state.quantity;
-    //     addOne++;
-    //     this.setState({quantity: addOne});
-    //     // const updateQuantity = {
-    //     //     quantity: addOne
-    //     // }
-    //     //const updatedFood = await foodsApi.updateFood(unique_foodId, updateQuantity);
-    //     //console.log(updatedFood);
-    //     //this.setState({ order_foodlist: updatedFood });
-    //     //console.log(this.state.order_foodlist);
-    // }
-
-
-
-    // decrement = () => {
-    //     let minusOne = this.state.quantity;
-    //     if (minusOne > 1) minusOne--;
-    //     this.setState({ quantity: minusOne });
-    //     //console.log(this.state.quantity);
-    // }
 
     handleChange = (e) => {
         console.log(e.target.name);
@@ -60,24 +12,6 @@ class Order extends Component {
             [e.target.name]: e.target.value
         });
     }
-
-    // deleteFood = async () => {
-    //     console.log('hi');
-    //     const obj = {
-    //         orderIdx: this.props.orderIdx,
-    //         foodIdx: this.props.idx
-    //     }
-    //     const foodListCopy = this.state.order_foodlist;
-    //     const deletedFood = await foodsApi.deleteFood(this.props.food._id, obj);
-    //     console.log(deletedFood);
-    //     const updatedFoodlist = foodListCopy.filter((foodObj) => {
-    //         return foodObj.food_id !== deletedFood[0].food_id;
-    //     }) ;
-    //     console.log(updatedFoodlist);
-    //     console.log(this.state);
-    //     this.setState({order_foodlist: updatedFoodlist});
-        
-    // }
 
     render() {
 
