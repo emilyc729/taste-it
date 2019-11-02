beb5fcc98190d94f231b23536afed85867f784d4
Emily <echeun17@mail.ccsf.edu>
2019-11-02 13:34:14 -0700

    fixed cors issue

--- src/services/menus-api.js
+++ src/services/menus-api.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://menunav.herokuapp.com/api/v1/'
 async function getAllMenus() {
-    let results = await fetch(`https://cors-anywhere.herokuapp.com/${API_URL}`, {mode: 'cors'})
+    let results = await fetch(API_URL, {mode: 'cors'})
         .then(res => res.json());  
     return results;
 }
