4c978536614ff6ae2153e8d2eefcb169c21b2319
Emily <echeun17@mail.ccsf.edu>
2019-11-02 13:38:50 -0700

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = (props) => {
 
     :
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to='' className="navbar-brand" href="#navbar"><i className="far fa-grin-wink"></i> Taste-It</Link>
+        <Link to='' className="navbar-brand"><i className="far fa-grin-wink"></i> Taste-It</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
