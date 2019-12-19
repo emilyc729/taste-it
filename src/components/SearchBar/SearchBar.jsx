dacb6873baf2b983ffa1703db0258b136d142a80
Emily <echeun17@mail.ccsf.edu>
2019-12-19 14:43:17 -0800

    added search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = (props) => {
 
     return (
         <div className="col-md-12 mb-5 d-flex">
-            <input className="form-control mr-2" type="search" placeholder="Search restaurant, food type..." aria-label="Search" />
+            <input className="form-control mr-2" type="search" placeholder="COMING SOON: Search restaurant, food type..." aria-label="Search" />
             <Link to="/" className="btn btn-outline-success"><i className="fas fa-search"></i></Link>
         </div>
     );
